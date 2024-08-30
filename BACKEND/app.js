0b42bb4662e08505d7d7af38befe48799074e6b6
etarise <etarise@gmail.com>
2024-08-30 10:39:05 +0100

    First Commit

---
+++ BACKEND/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+require("dotenv/config");
+
+const api = process.env.API_URL;
+
+app.get(`${api}/products`, (req, res) => {
+  const product = { id: 1, name: "hair dresser", image: "some_url " };
+  res.send(product);
+});
+
+app.listen(3000, () => {
+  console.log(api);
+  console.log("server is running at htt://localhost:3000");
+});
